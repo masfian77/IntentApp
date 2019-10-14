a352254912468e86ed7521cf9e46f487caed7491
ALFIAN SATRIA V.P <masfian77@gmail.com>
2019-10-14 13:34:27 +0700

    committed

---
+++ app/src/main/java/com/alfian/intentapp/MoveActivity.kt
@@ -0,0 +1,12 @@
+package com.alfian.intentapp
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MoveActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_move)
+    }
+}
